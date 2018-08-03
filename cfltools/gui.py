572f86b9c2248dba9bfdef4282df94e74e43a08e
Bradley Evans <bevan006@ucr.edu>
2018-08-03 10:35:00 -0700

    Baby's First GUI

--- cfltools/gui.py
+++ cfltools/gui.py
@@ -1,13 +1,40 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
 
-class TestGui(App):
+
+# class ScreenStart(Screen):
+#     pass
+
+
+# class ScreenLogparse(Screen):
+#     pass
+
+
+# class MainMenu(FloatLayout):
+#     manager = ObjectProperty(None)
+
+
+class Manager(BoxLayout):
+
+    sm = ObjectProperty()
+
+    def set_previous_screen(self):
+        if self.sm.current != 'start':
+            self.sm.transition.direction = 'right'
+            self.sm.current = self.sm.previous()
+
+
+class CFLToolsApp(App):
 
     def build(self):
-        return Label(text='Hello, world!')
+        return Manager()
+
 
 def gui():
     print('In gui().')
-    TestGui().run()
+    CFLToolsApp().run()
