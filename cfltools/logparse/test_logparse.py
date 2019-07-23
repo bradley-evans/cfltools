65b11c6912fa758ebe71088ebdc893226a35a8a6
Bradley Evans <squelch.21@zoho.com>
2019-07-23 15:27:00 -0700

    Some dates in tests were given as PST, corrected these dates to the appropriate UTC time.

--- cfltools/logparse/test_logparse.py
+++ cfltools/logparse/test_logparse.py
@@ -204,10 +204,10 @@ def test_csvfile_findunique(logfile_unprocessed):
 
 def test_ipaddress_timestampconversion():
     times = [
-        'Wed Dec 31 16:16:40 1969',
-        'Wed Dec 31 16:33:20 1969',
-        'Wed Dec 31 16:50:00 1969',
-        'Wed Dec 31 17:06:40 1969'
+        'Thursday, January 1, 1970 12:16:40 AM',
+        'Thursday, January 1, 1970 12:33:20 AM',
+        'Thursday, January 1, 1970 12:50:00 AM',
+        'Thursday, January 1, 1970 1:06:40 AM'
         ]
     ipaddr = IPAddress('0.0.0.0', 'Wed Dec 31 16:25:00 1969')
     for time in times:
